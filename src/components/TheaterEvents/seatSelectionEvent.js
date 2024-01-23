9da590aaa2397eadb0605144c340473e82b6d593
songhee <zxy4826@naver.com>
2024-01-23 22:06:17 +0900

    좌석 선택시 가격표시

--- src/components/TheaterEvents/seatSelectionEvent.js
+++ src/components/TheaterEvents/seatSelectionEvent.js
@@ -18,10 +18,12 @@ export const seatSelectionEvent = (seatBuff, target) => {
     seatBuff.push(0);
     seatBuff.push(0);
   }
+  //길이가 2보다 작을경우에만 0으로 초기화하고, 그 외의 경우에는 아예 조건에 해당하지 않게 해놓음.
 
   let tartgetList = target.classList;
   console.log(tartgetList);
   let price = 0;
+  //최종적으로 계산될 가격이 들어감
   if (tartgetList.contains("handicap")) {
     price = selectHandicapHandler(target);
   } else if (tartgetList.contains("musseukbox")) {
@@ -29,21 +31,30 @@ export const seatSelectionEvent = (seatBuff, target) => {
   } else {
     price = selectGeneralHandler(target, seatBuff);
   }
+  //타겟이 어떤걸 포함하고있냐에 따라 해당함수로 이동, 가격이 달라짐
 
   const enableSeatsList = document.querySelectorAll(
     "button.seat:not(.disabled)"
   );
   enableSeatsList.forEach((elem) => elem.classList.remove("disabled"));
+  //초기에 "disabled" 클래스가 없어도, 이후에 프로그램 실행 도중에 해당 요소들에 "disabled" 클래스가 추가될 수 있기 때문
+  //이렇게 하면 enableSeatsList에는 여전히 "disabled" 클래스를 가지지 않은 좌석 요소만이 남아 있게 됨
 
   let numOfMoviegoers = numOfAdult() + numOfYouth();
+  //총 관람객 수
   let clickedSeatLen = document.querySelectorAll(".clicked").length;
+  //사용자가 선택한 좌석의 수
 
+  //관람객 수와 사용자가 선택한 좌석의 수가 같다면 clicked가 포함되지 않은거에는 disabled를 추가.
   if (clickedSeatLen == numOfMoviegoers) {
     enableSeatsList.forEach((elem) => {
       if (!elem.classList.contains("clicked")) elem.classList.add("disabled");
     });
-  } else if (clickedSeatLen == 0) {
-    // 관람 인원에 따른 장애인 체크박스 및 장애인석 활성화/비활성화
+  }
+
+  //아직 좌석을 선택하지 않은 경우
+  //관람 인원에 따른 장애인 체크박스 및 장애인석 활성화/비활성화
+  else if (clickedSeatLen == 0) {
     if (numOfMoviegoers > 3) {
       generalHandler(true);
       musseukboxHandler(true);
--- src/components/TheaterHandlers/seatSelectionHandler.js
+++ src/components/TheaterHandlers/seatSelectionHandler.js
@@ -10,6 +10,8 @@ import {
 const calculatePrice = (target, seatBuff, discount) => {
   let price = 0;
   let clickedSeatLen = document.querySelectorAll(".clicked").length;
+
+  //타켓에
   if (target.classList.contains("clicked")) {
     if (numOfAdult() != seatBuff[0]) {
       price = 10000 * discount;
@@ -35,13 +37,17 @@ export const selectMussukboxHandler = (target, seatBuff) => {
   let price = 0;
 
   let numOfMoviegoers = numOfAdult() + numOfYouth();
+  //사용자가 선택한 관람 인원수 계산
 
   if (!parseInt(numOfMoviegoers % 2) == 0) {
+    //관람인원이 짝수가 아닐 경우, 즉 홀수일 경우 경고 알림창이 뜨면서 가격은 0으로 설정됨.
     price = 0;
     alert(
       "선택하신 'MUSSEUKBOX' 좌석은 2인 좌석입니다. 2인 단위로 인원을 선택해주세요."
     );
-  } else {
+  }
+  //짝수일 경우
+  else {
     target.classList.toggle("clicked");
     generalHandler(false); // 일반석 비활성화
     handicapHandler(false); // 장애인석 비활성화
@@ -55,20 +61,27 @@ export const selectMussukboxHandler = (target, seatBuff) => {
 };
 export const selectHandicapHandler = (target) => {
   let price = 5000; // 1
+  //장애인은 성인, 청소년 상관없이 5000원으로되어있음.
   const checkHandicap = document.querySelector("#checkHandicap"); // 2
 
   // 3
+  //장애인 체크박스가 체크되었는지 여부에 따라 달라짐.
   if (!checkHandicap.checked) {
     // 3 - 1
     price = 0;
     alert("선택하신 좌석은 장애인석으로 일반고객은 예매할 수 없는 좌석입니다.");
-  } else {
+  }
+  //체크박스가 체크되었다면 장애인석은 활성화가 되고 선택가능해짐.
+  //그리고 원하는 좌석 선택시 clicked가 생김
+  //좌석을 다 선택 후 다른 좌석들은 disabled가 생기면서 비활성화됨.
+  else {
     // 3 - 2
     target.classList.toggle("clicked");
     handicapHandler(true);
   }
 
   return price;
+  //최종 계산 가격 반환
 };
 
 export const selectGeneralHandler = (target, seatBuff) => {
